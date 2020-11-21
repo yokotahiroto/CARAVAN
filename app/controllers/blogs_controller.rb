b75e3b8d8ca6eb9cf96cf47014c176670a0076f3
yokotahiroto <hy_dkbs66mh@icloud.com>
2020-11-21 07:44:27 +0000

    [start]5-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,12 +4,16 @@ class BlogsController < ApplicationController
 
   def show
   end
+  
+  def index
+    @blogs = Blog.all
 
   def new
+    @blog = Blog.new
   end
   
   def create
-    @blog = Blog.new(blog_params)
+    blog = Blog.new(blog_params)
     blog.save
     redirect_to blogs_path
   end
